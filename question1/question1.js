66a8f3850da54b7cf78c58319b8b0999fb3c2ca9
sarayousifm1105 <sarahyousif.h22@gmail.com>
2021-01-03 20:30:55 +0300

    Update question1.js

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+function sayhello(name){
+    
+    if(name!=null)
+    console.log(`hello${name}`);
+
+    else{
+        console.log(`pease enter a name`);
+}
+}
+sayhello(`sara`);
+sayhello();
